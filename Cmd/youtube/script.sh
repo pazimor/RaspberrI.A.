0469eea982fef826b85b38169fb8b3e595d551d6
hummel_e <eddy.hummel@epitech.eu>
2018-03-12 09:16:25 +0100

    adding some functions

---
+++ Cmd/youtube/script.sh
@@ -0,0 +1,1 @@
+firefox https://www.youtube.com
--- IA/launcher.py
+++ IA/launcher.py
@@ -1,6 +1,10 @@
+#!/usr/bin/python3
+
 import snowboydecoder
 import sys
 import signal
+import os
+import string
 
 interrupted = False
 
@@ -14,11 +18,50 @@ def interrupt_callback():
 
 #= start we have to load all models here
 #
+said = []
+script = []
+Hotword = []
+
+def storeWords(word):
+    said.append(word)
+
+def getCallbackArray(array):
+    callbacks = []
+    for i in array:
+        callbacks.append(lambda: storeWords(i))
+    return callbacks
+
+def getVocabulary(path):
+    files = [f.split(".")[0] for f in os.listdir(path) if (os.path.isfile(os.path.join(path, f)) and f.find(".pmdl") != -1)]
+    print(files)
+    return files
+
+def getCommande(path):
+    files = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
+    return files
+
+def getFolders(path):
+    files = [f for f in os.listdir(path) if os.path.isdir(os.path.join(path, f))]
+    return files
+
+
+array = getVocabulary(os.getcwd() + "/../vocabulary")
+callbacks = getCallbackArray(array)
+callbacks[1]()
+print(said)
+folders = getFolders(os.getcwd() + "/../Cmd")
+for i in folders:
+    commande = getCommande(os.getcwd() + "/../Cmd/" + str(i))
+    if commande[0] == "script.sh":
+        script.append(os.getcwd() + "/../Cmd/" + str(i) + "/" + commande[0])
+    if commande[1] == "sentence":
+        Hotword.append(os.getcwd() + "/../Cmd/" + str(i) + "/" + commande[1])
+print(script)
+print(Hotword)
 
 print("work in progress")
 sys.exit(-1)
 
-# a model is a path :) recursivity is comming 
 models = sys.argv[1:]
 
 # capture SIGINT signal, e.g., Ctrl+C
